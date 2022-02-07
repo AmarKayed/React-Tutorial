ee7ab15ca5be700dfed83a8c3875e52d7af87817
Amar Kayed <amar.kayed18@gmail.com>
2022-02-07 13:37:05 +0200

    Added Second Counter Button Vanilla JS

--- intro/index.js
+++ intro/index.js
@@ -6,3 +6,14 @@ console.log(result, button)
 button.addEventListener('click', ()=>{
     result.innerHTML = parseInt(result.innerHTML) + 1
 })
+
+
+
+const result2 = document.querySelector('#count-display2');
+const button2 = document.querySelector('#increment-btn2');
+
+console.log(result2, button2)
+
+button2.addEventListener('click', ()=>{
+    result2.innerHTML = parseInt(result2.innerHTML) + 1
+})
