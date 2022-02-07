d227a714d9ba9ddb0c27e753cfbf4e52aaba526a
Amar Kayed <amar.kayed18@gmail.com>
2022-02-07 15:57:57 +0200

    Added useState Hook for the CountButton Incrementation

--- react_frontend/src/components/CountButton.js
+++ react_frontend/src/components/CountButton.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { useState } from "react";
+
+
 
 const CountButton = () => {
+    const [currentCount, setCurrectCount] = useState(0)
+
+    const handleClick = () => {
+        setCurrectCount(currentCount + 1)
+    }
     return (
         <div>
-            <button>+1</button>
-            <div>0</div>  
+            <button onClick={handleClick}>+1</button>
+            <div>{currentCount}</div>  
         </div>
     )
 }
